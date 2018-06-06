426d0fa115bcb16a041f6b5f3249e8b115cddcda
jusjusjus <jschwabedal@gmail.com>
2018-06-06 09:38:33 -0400

    Made load-model async.

--- js/sleep_staging_model.js
+++ js/sleep_staging_model.js
@@ -6,34 +6,43 @@ function assert(condition, msg) {
   }
 }
 
-function readTextFile(file, callback) {
+function readPlainText(file) {
+  return new Promise(function (resolve, reject) {
+    var response = null;
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
     rawFile.open("GET", file, true);
     rawFile.onreadystatechange = function() {
         if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
+            resolve(rawFile.responseText);
         }
     }
     rawFile.send(null);
+  });
 }
 
 var Model = function (self) {
   self = self || {};
+  self.config = null;
   self.net = null;
 
   function load_config(filename) {
-    readTextFile(filename, async function(text) {
-        self.config = JSON.parse(text);
+    return new Promise(function (resolve, reject) {
+      readPlainText(filename).then( function (txt) {
+        var txt = JSON.parse(txt);
+        resolve(txt);
+        self.config = txt;
+      });
     });
   }
 
   async function load_model(num_layers=8) {
-    if (self.net) { console.log("Warning re-loading net"); }
+    assert(self.net === null, "Warning re-loading net");
     var modelfile = '../models/humans/production/num_layers_'+num_layers+'/model/model.json'
     var configfile = '../models/humans/production/num_layers_'+num_layers+'/model/config.json'
-    self.load_config(configfile);
-    self.net = await tf.loadModel(modelfile);
+    self.config = load_config(configfile);
+    self.net = tf.loadModel(modelfile);
+    await Promise.all([self.config, self.net]);
   }
 
   function input_sampling_rate() {
@@ -49,7 +58,6 @@ var Model = function (self) {
     return output;
   }
   self.load_model = load_model;
-  self.load_config = load_config;
   self.predict = predict;
   return self;
 }
